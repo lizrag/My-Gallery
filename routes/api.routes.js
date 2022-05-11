c7605a0cef95ae1879619651380b629a4cb2520a
lizrag <perlizy.1996@gmail.com>
2022-05-11 17:08:23 -0500

    Add CSS

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
+
+const upload= multer();
 const apiRoutes = Router();
 
-apiRoutes.post('/upload', ()=>{
+apiRoutes.post('/photos/upload', upload.array('photos', 6), function (req, res) {
 
-});
+})
 
 apiRoutes.delete('/delete', ()=>{
 
