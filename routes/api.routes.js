2ba108521a1583ca9f836516d837ccb1ce24dcc6
lizrag <perlizy.1996@gmail.com>
2022-05-12 21:21:04 -0500

    Add multer

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,16 +1,30 @@
 import { Router } from "express";
 import multer from "multer";
+import fs from 'fs'; 
 
-
-const upload= multer();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/photos')
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now()+ file.originalname)
+    },
+  });
+  
+const upload = multer({ storage: storage })
 const apiRoutes = Router();
 
 apiRoutes.post('/photos/upload', upload.array('photos', 6), function (req, res) {
-
+    console.log(req.files);
+    return;
 })
-
-apiRoutes.delete('/delete', ()=>{
-
+/*
+apiRoutes.delete('/delete:filename', ()=>{
+  let deletePhoto = req.params.filename;
+  fs.unlink(deletePhoto);
+  return res.send('Photo deleted');
 });
+*/
+
 
 export default apiRoutes;
